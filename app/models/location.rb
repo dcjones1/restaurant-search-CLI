662d94c8ebcecb77bbd18f6e5f90d38163453f93
Ben Yellin <byellindc@gmail.com>
2019-01-24 12:26:42 -0500

    fix location nearby

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,16 +2,28 @@ class Location < ActiveRecord::Base
   has_many :restaurants
 
   def nearby_restaurants(mile_radius)
+    if self.latitude.nil? || self.longitude.nil?
+      return []
+    end
+
     Restaurant.all.select do |restaurant|
+      if restaurant.latitude.nil? || restaurant.longitude.nil?
+        next
+      end
+
       distance = distance_between(self.latitude, self.longitude, restaurant.latitude, restaurant.longitude)
       distance <= mile_radius
     end
   end
 
   def nearby_locations(mile_radius)
+    if self.latitude.nil? || self.longitude.nil?
+      return []
+    end
+
     Location.all.select do |location|
       distance = distance_between(self.latitude, self.longitude, location.latitude, location.longitude)
-      distance <= mile_radius
+      location != self && distance <= mile_radius
     end
   end
 end
