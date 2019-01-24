92eb6c519ca9445df5eb5af2ac8915c4bdae9bdc
dcjones1 <dcejjones@gmail.com>
2019-01-24 17:35:39 -0500

    working model, all functions work and catches errors

--- bin/runner.rb
+++ bin/runner.rb
@@ -4,33 +4,42 @@ require_relative 'cli_methods.rb'
 def runner(inputs = [])
   while true
     location = greeting
-    location = Location.find_by(title: location)
-    input = main_menu
-  
-    case input
-    when 1
-      restaurants = average_cost(location)
-    when 2
-      category = list_categories(location)
-      restaurants = location.restaurants_with_category(category)
-    when 3
-      restaurant = find_restaurant()
+    if location.downcase.starts_with?('x')
+      puts "\n#{Location.all.map(&:title).join(", ")}\n\n"
+    else
+      location = Location.all.find do |locality|
+        String::Similarity.levenshtein(locality.title, location) >= 0.3
+      end
+      if !location.nil?
+        input = main_menu
+
+        case input
+        when 1
+          restaurants = average_cost(location)
+        when 2
+          category = list_categories(location)
+          restaurants = location.restaurants_with_category(category)
+        when 3
+          restaurant = find_restaurant()
+        end
+
+        if restaurants.any?
+          restaurant = list_restaurants(restaurants)
+        end
+
+        if restaurant
+          show_restaurant(restaurant)
+        end
+
+        puts "\nDo you want to perform another search? (y/n)"
+
+        input = inputs.shift || gets.chomp
+        break if input.downcase.starts_with?('n')
+      else
+        puts "\nPlease enter a valid neighborhood"
+      end
     end
-
-    if restaurants.any?
-      restaurant = list_restaurants(restaurants)
-    end
-
-    if restaurant
-      show_restaurant(restaurant)
-    end
-
-    puts "\nDo you want to perform another search? (y/n)"
-
-    input = inputs.shift || gets.chomp
-    break if input.downcase.starts_with?('n')
   end
 end
-
 
 runner
