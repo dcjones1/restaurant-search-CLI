ab6b529907899c969654e1dcbc5bd4aee3dbb383
Ben Yellin <byellindc@gmail.com>
2019-01-24 10:39:52 -0500

    fix helper spec

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -1,7 +1,7 @@
 require_relative 'spec_helper'
 
 describe "Helper" do
-  it "calculates mile distance between lat/long coordinates"
+  it "calculates mile distance between lat/long coordinates" do
     expect(distance_between(77, 1, 78, 1)).to eq(69.05)
   end
 end
